00c329e5e9f607975917e2c66f7b0a85e7aaed21
Aatif70 <ideapadaatif@gmail.com>
2024-10-28 11:15:46 +0530

    a lot of changes

---
+++ Ai Expense Tracker/Views/FilterCategoriesView.swift
@@ -0,0 +1,19 @@
+//
+//  FilterCategories.swift
+//  Ai Expense Tracker
+//
+//  Created by Aatif Ahmed on 10/28/24.
+//
+
+import SwiftUI
+
+struct FilterCategories: View {
+    var body: some View {
+        Text("Filter Categories")
+    }
+}
+
+
+#Preview {
+    FilterCategories()
+}
